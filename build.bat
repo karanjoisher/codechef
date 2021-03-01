9876911894baee0544b5d149cc7c141deb0cc97b
Karan Joisher <karanjoisher@gmail.com>
2021-03-01 21:29:03 +0530

    Solved 'Entrance Exam' problem (ENTEXAM)

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set compiler_flags=-FC -MD -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -wd4127 -wd4505 -wd4189 -fp:fast -Z7 -Od 
+set compiler_flags=-FC -MD -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -wd4127 -wd4505 -wd4189 -fp:fast -Z7 -Od -EHsc
 
 if not exist build\. mkdir build
 pushd build
